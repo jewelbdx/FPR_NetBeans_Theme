ee2faeb0385243b4ba38c93f4efd0251d0d6ddf8
JEWEL AHMMED <jewelbdx@gmail.com>
2017-09-12 04:39:06 +0600

    Initial Commit!

---
+++ Others/FPR_full_img.php
@@ -0,0 +1,14 @@
+<?php
+
+$Quotations = 'Please Help A Poor Child!';
+$You = 5; // Dollar
+$FriendsOrFamily = 5;
+
+$Total = $You + $FriendsOrFamily;
+
+if ($Total >= 10) {
+
+    echo 'You can buy some foods for a poor child... :)';
+} else {
+    echo 'Please try to manage more dollar for buying some food!';
+}
